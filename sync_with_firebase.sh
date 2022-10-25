9d01365f8f1e3f1c89d23366b67810a5850bbb0d
Patrick Drechsler <patrick@pdrechsler.de>
2022-10-25 21:40:48 +0200

    add useful flags (currently not needed) for debugging bash script

--- sync_with_firebase.sh
+++ sync_with_firebase.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# set -x ## Use for debugging
+# set -e ## Use for catching exceptions
+
 ## https://stackoverflow.com/a/246128
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
 
